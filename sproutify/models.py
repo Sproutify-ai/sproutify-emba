195ea3fb18245633bc0e3a18da6037ebb54a3b3d
Ian Chen <ianchen06@gmail.com>
2024-04-03 12:11:19 -0700

    fix user model

--- sproutify/models.py
+++ sproutify/models.py
@@ -4,6 +4,6 @@ from . import db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.Text())
     name = db.Column(db.String(1000))
     is_admin = db.Column(db.Boolean, default=False)
