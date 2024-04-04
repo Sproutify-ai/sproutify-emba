7ebcb517fb625395f1b9280c383366fd3966f9a0
Ian Chen <ianchen06@gmail.com>
2024-04-03 23:05:50 -0700

    update date, add solutions

--- sproutify/main.py
+++ sproutify/main.py
@@ -1,5 +1,9 @@
+import os
+
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+import pandas as pd
+
 from . import db
 
 main = Blueprint("main", __name__)
@@ -12,3 +16,20 @@ def index():
 @login_required
 def profile():
     return render_template("profile.html.j2", name=current_user.name)
+
+@main.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html.j2", name=current_user.name)
+
+@main.route("/solutions")
+@login_required
+def index_solutions():
+    csv_path = os.path.join(os.path.dirname(__file__), 'static/csv/solutions.csv')
+    df = pd.read_csv(csv_path)
+    return render_template("solutions_index.html.j2", titles=df['Provide a one-line summary of your solution.'])
+
+@main.route("/solutions/<id>")
+@login_required
+def show_solutions(id):
+    return render_template("solutions_show.html.j2", id=id)
