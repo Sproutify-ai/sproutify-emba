6dc1e4734bdb25702a0fa621ffbab8839672d77e
Leonard Boussioux <leobix@mit.edu>
2025-09-27 12:39:06 -0700

    config

--- sproutify/config.py
+++ sproutify/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'  
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///sproutify.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.environ.get('DEBUG', False)  # Cast to boolean
